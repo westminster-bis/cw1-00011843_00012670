711fdcdaef0eea88a48c23a7e4ae0d5feff70f08
00011843 <wiut00011843@gmail.com>
2021-12-01 12:45:35 +0500

    add a function that checks the user's answer is yes or no or gets invalid result

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ name = input("First of all how can I call you: ")
 greeting = f'Hey {name} 👋. How are you doing? and your study in WIUT?'
 
 print(greeting)
+
+def answer_to_question(answer):
+    """ 
+    The function that if user enters "No", (alternatives for No: "NO", "nO", "N" or just "n") it will return False. 
+    When the user enters "Yes" (alternatives for Yes: "YES", "YEs", "yES" "Y" or just "y") it will return True.
+    In another case when the user enters the invalid answer it will repeat the question. 
+
+    """
+    while answer:
+        answer = answer.lower()
+        if answer == 'yes' or answer == 'y':
+            return True
+        elif answer == 'no' or answer == 'n':
+            return False
+        else:
+            answer = input("Oops. Sorry I didn't get your answer. Please, write yes or no: ")
