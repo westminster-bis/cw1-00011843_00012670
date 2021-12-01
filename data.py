4dfe95a942583369fccab50dcd97c1bca67e4606
Converter0 <kazimovadinora@gmail.com>
2021-12-01 13:03:56 +0500

    add all necessary data into dictionary and other file

---
+++ data.py
@@ -0,0 +1,25 @@
+modules = {
+    "Computer Science": "01/12/2021 11:59:59 PM",
+    "Web Technology": "09/12/2021 11:59:59 PM",
+    "IMOB": "22/11/2021 11:59:59 PM",
+    #when we learn web parsing with beatiful soup package in python, we will implement it there to get data from intranet.wiut.uz
+}
+
+submitted_time = {
+    "on_time": "Great! You've submitted it in on time. Don't worry about it.",
+    "within_24": "Yo've submitted your CW within 24 hours after the submission deadline",
+    "within_5_days": "You've submitted your CW within 5 days",
+    "more_than_5_days": "Yo've submitted your CW more than 5 days"
+}
+
+questions_reason = {
+    "is_valid_reason": "Your CW will be assigned as a late submission. So is there valid reason to submit lately? ",
+    "mc_claim_acceptance": "You supposed to submit your MC claim. Did they accept your MC claim? ",
+}
+
+results = {
+    "full_mark": "Perfect! You will get a full mark based on assesment criteria.",
+    "minus_10": "Unfortunately, you will get minus 10 mark from overall mark, but not below 40.",
+    "zero_mark": "You've failed for this module in this semester. Overall you will get 0 mark in total.",
+    "deferral": "Unfortunately, you've assigned to deferral reassesment in summer."
+}
